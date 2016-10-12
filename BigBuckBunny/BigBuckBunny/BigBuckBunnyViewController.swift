34624de6e96eac6dc1d794a6f8a021bdda8133c0
David Cordero <dcorderoramirez@gmail.com>
2016-10-12 14:32:33 +0200

    Minor change

--- BigBuckBunny/BigBuckBunny/BigBuckBunnyViewController.swift
+++ BigBuckBunny/BigBuckBunny/BigBuckBunnyViewController.swift
@@ -30,12 +30,12 @@ class BigBuckBunnyViewController: AVPlayerViewController {
     
     private func metaData() -> [AVMetadataItem] {
         var metadataItems: [AVMetadataItem] = []
-        let metadata = createMetadataItem(AVMetadataCommonIdentifierTitle, value: "Big Buck Bunny")
-        metadataItems.append(metadata)
         
-        let description = createMetadataItem(AVMetadataCommonIdentifierDescription, value: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from 'de Finibus Bonorum et Malorum' by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.")
+        let metadataTitle = createMetadataItem(AVMetadataCommonIdentifierTitle, value: "Big Buck Bunny")
+        metadataItems.append(metadataTitle)
         
-        metadataItems.append(description)
+        let metadataDescription = createMetadataItem(AVMetadataCommonIdentifierDescription, value: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from 'de Finibus Bonorum et Malorum' by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.")
+        metadataItems.append(metadataDescription)
         
         return metadataItems
     }
